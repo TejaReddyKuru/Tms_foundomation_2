6a458c237fbe47fd793bd96dcf2eea81a8ed59a1
teja <kteja0816@gmail.com>
2026-07-14 15:54:24 +0530

    Remove backend

--- backend/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Hero, About, Quote, Contact
-
-admin.site.register(Hero)
-admin.site.register(About)
-admin.site.register(Quote)
-admin.site.register(Contact)
--- backend/test_api.py
+++
@@ -1,9 +0,0 @@
-import urllib.request, json, urllib.error, re
-req = urllib.request.Request('https://backend-pi-lovat-57.vercel.app/api/auth/register/', data=json.dumps({'username':'teja', 'email':'Testing@123', 'password':'password123'}).encode('utf-8'), headers={'Content-Type': 'application/json'})
-try:
-    urllib.request.urlopen(req)
-except urllib.error.HTTPError as e:
-    html=e.read().decode('utf-8')
-    with open('error.html', 'w') as f:
-        f.write(html)
-    print("Error saved to error.html")
