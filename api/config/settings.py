d4111a2517d4ba2011cfe4f162e992c97ee17173
teja <kteja0816@gmail.com>
2026-07-14 17:23:35 +0530

    Fix Vercel python app routing and serve Django static files

--- api/config/settings.py
+++ api/config/settings.py
@@ -98,7 +98,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../public/static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
