f54ac09d2265d9198915958c020c0e2568d9b66f
teja <kteja0816@gmail.com>
2026-07-14 17:42:48 +0530

    Add skipTrailingSlashRedirect to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  skipTrailingSlashRedirect: true,
   async rewrites() {
     return [
-      {
-        source: "/api/:path*",
-        destination: "/api/:path*", // Passes through to Vercel's API folder
-      },
       {
         source: "/admin/:path*",
         destination: "/api/admin/:path*", // Forwards /admin to Django in /api/
